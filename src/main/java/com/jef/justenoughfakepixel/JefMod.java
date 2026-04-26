e397d165b8cd01669af469260cf6318182b6e42b
hamlook <hamlockgit@gmail.com>
2026-04-26 19:37:13 +0500

    version bump holy fuck

--- src/main/java/com/jef/justenoughfakepixel/JefMod.java
+++ src/main/java/com/jef/justenoughfakepixel/JefMod.java
@@ -34,7 +34,7 @@ public class JefMod {
 
     public static final String MODID = "justenoughfakepixel";
     public static final String NAME = "JustEnoughFakepixel";
-    public static final String VERSION = "1.2.6";
+    public static final String VERSION = "1.2.7";
 
     public static JefConfig config;
     public static Logger logger;
